2c5a05bf08b12aa194a548ae9d4ddaeb5da46c32
KimCookieYa <min49590@gmail.com>
2024-10-04 19:46:51 +0900

    init: pwa 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import PushNotificationManager from '@/app/_component/PushNotificationManager';
+import InstallPrompt from '@/app/_component/InstallPrompt';
+
+
 export default function Home() {
   return (
-    <main>Hello, World!</main>
+    <main>Hello, World!
+            <PushNotificationManager />
+            <InstallPrompt />
+    </main>
   );
 }
